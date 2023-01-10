3823175c7ca0ab96cf36e49c3bae1939667ad2a9
Sabbirz <sabbir.kuasha@gmail.com>
2023-01-10 15:34:39 +0600

    svelte store with local storage

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,13 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
-export const userName = writable('sabbir');
+// this will only store the data in svelte store
+// export const userName = writable('sabbir');
+
+// This will store the data both local storage and svelte store
+// This line means we are geting the item name userName from localStorage but if the item is not there the system will pick sabbir, in this case
+//string
+export const userName = writable(
+	(browser && localStorage.getItem('userName')) || 'No Data Available'
+);
+userName.subscribe((val) => browser && (localStorage.userName = val));
